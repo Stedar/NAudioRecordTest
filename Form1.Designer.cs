c593bf34639602f99ffab5fe8ce4705b8bef9c8e
S. Beloyar <velemir.s@gmail.com>
2020-12-26 23:37:04 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace NAudioRecordTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ButtonStop = new System.Windows.Forms.Button();
+            this.ButtonRecord = new System.Windows.Forms.Button();
+            this.progressBarVolume = new System.Windows.Forms.ProgressBar();
+            this.RecognizeResultTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // ButtonStop
+            // 
+            this.ButtonStop.Location = new System.Drawing.Point(149, 12);
+            this.ButtonStop.Name = "ButtonStop";
+            this.ButtonStop.Size = new System.Drawing.Size(75, 23);
+            this.ButtonStop.TabIndex = 1;
+            this.ButtonStop.Text = "Stop";
+            this.ButtonStop.UseVisualStyleBackColor = true;
+            this.ButtonStop.Visible = false;
+            this.ButtonStop.Click += new System.EventHandler(this.ButtonStop_Click);
+            // 
+            // ButtonRecord
+            // 
+            this.ButtonRecord.Location = new System.Drawing.Point(68, 12);
+            this.ButtonRecord.Name = "ButtonRecord";
+            this.ButtonRecord.Size = new System.Drawing.Size(75, 23);
+            this.ButtonRecord.TabIndex = 2;
+            this.ButtonRecord.Text = "Record";
+            this.ButtonRecord.UseVisualStyleBackColor = true;
+            this.ButtonRecord.Click += new System.EventHandler(this.ButtonRecord_Click);
+            // 
+            // progressBarVolume
+            // 
+            this.progressBarVolume.Location = new System.Drawing.Point(68, 49);
+            this.progressBarVolume.Name = "progressBarVolume";
+            this.progressBarVolume.Size = new System.Drawing.Size(498, 23);
+            this.progressBarVolume.TabIndex = 3;
+            // 
+            // RecognizeResultTextBox
+            // 
+            this.RecognizeResultTextBox.Location = new System.Drawing.Point(68, 91);
+            this.RecognizeResultTextBox.Multiline = true;
+            this.RecognizeResultTextBox.Name = "RecognizeResultTextBox";
+            this.RecognizeResultTextBox.Size = new System.Drawing.Size(498, 173);
+            this.RecognizeResultTextBox.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.RecognizeResultTextBox);
+            this.Controls.Add(this.progressBarVolume);
+            this.Controls.Add(this.ButtonRecord);
+            this.Controls.Add(this.ButtonStop);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button ButtonStop;
+        private System.Windows.Forms.Button ButtonRecord;
+        private System.Windows.Forms.ProgressBar progressBarVolume;
+        private System.Windows.Forms.TextBox RecognizeResultTextBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio;
+using NAudio.Wave;
+using NAudio.FileFormats;
+using NAudio.CoreAudioApi;
+using System.IO;
+using Google.Cloud.Speech.V1;
+
+
+namespace NAudioRecordTest
+{
+
+    public partial class Form1 : Form
+    {
+        WaveInEvent waveIn;
+        String outputFilePath;
+        WaveFileWriter writer;
+        float max_v; //for volume
+        bool isWriting;
+        float TimeDelay;
+        DateTime LastWritingDateTime;
+        SpeechClient speech;
+        RecognitionConfig config;
+        String strRecgnResult;
+ 
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            var outputFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "NAudio");
+            Directory.CreateDirectory(outputFolder);
+            outputFilePath = Path.Combine(outputFolder, "recorded.wav");
+            waveIn = new WaveInEvent();
+            waveIn.WaveFormat = new WaveFormat(44100, 16, 1);
+            waveIn.DataAvailable += waveIn_DataAvailable;
+            waveIn.RecordingStopped += waveIn_RecordingStopped;
+
+            writer = null;
+            max_v = 0;
+            isWriting = false;
+            TimeDelay = 1; //seconds
+            strRecgnResult = "";
+
+            speech = SpeechClient.Create();
+            config = new RecognitionConfig
+            {
+               // Encoding = RecognitionConfig.Types.AudioEncoding.Flac,
+                //SampleRateHertz = 16000,
+                LanguageCode = LanguageCodes.Russian.Russia
+            };
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        void StopRecord()
+        {
+            waveIn.StopRecording();
+            isWriting = false;
+
+        }
+
+        void StartRecord()
+        {
+            writer = new WaveFileWriter(outputFilePath, waveIn.WaveFormat);
+            waveIn.StartRecording();
+            LastWritingDateTime = DateTime.Now;
+        }
+
+        private void ButtonStop_Click(object sender, EventArgs e)
+        {
+            ButtonRecord.Enabled = true;
+            StopRecord();
+        }
+
+        private void ButtonRecord_Click(object sender, EventArgs e)
+        {
+            StartRecord();
+        //    ButtonRecord.Enabled = false;
+          //  ButtonStop.Enabled = true;
+        }
+
+        void waveIn_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            TimeSpan ts = TimeSpan.Zero;
+            max_v = 0;
+            // interpret as 16 bit audio
+            for (int index = 0; index < e.BytesRecorded; index += 2)
+            {
+                short sample = (short)((e.Buffer[index + 1] << 8) |
+                                        e.Buffer[index + 0]);
+                // to floating point
+                var sample32 = sample / 32768f;
+                // absolute value 
+                if (sample32 < 0) sample32 = -sample32;
+                // is this the max value?
+                if (sample32 > max_v) max_v = sample32;
+            }
+
+            ts = DateTime.Now - LastWritingDateTime;
+
+            if (max_v > 0.1 || (isWriting & ts.Seconds < TimeDelay))
+            {
+                writer.Write(e.Buffer, 0, e.BytesRecorded);
+                if (max_v > 0.1)
+                    LastWritingDateTime = DateTime.Now;
+                if (!isWriting)
+                    isWriting = true;
+            }
+            else if (isWriting & (max_v <= 0.1 & ts.Seconds >= TimeDelay))
+            {
+                  StopRecord();
+            }
+
+            update_volume_progress_bar();
+        }
+
+        void update_volume_progress_bar()
+        {
+            if (progressBarVolume.InvokeRequired)
+                progressBarVolume.Invoke(new Action(update_volume_progress_bar));
+            else
+                progressBarVolume.Value = Convert.ToInt32(100 * max_v);
+        }
+
+
+        void waveIn_RecordingStopped(object sender, EventArgs e)
+        {
+            waveIn.Dispose();
+            writer.Close();
+            writer = null;
+            SpeechToText();
+        }
+
+        void SpeechToText()
+        {
+            strRecgnResult = "";
+            var audio = RecognitionAudio.FromFile(outputFilePath);
+
+            var response = speech.Recognize(config, audio);
+
+            foreach (var result in response.Results)
+            {
+                foreach (var alternative in result.Alternatives)
+                {
+                    strRecgnResult += ((alternative.Transcript) + "...");
+                }
+            }
+
+            RecognizeResultTextBox.Invoke((MethodInvoker)delegate { RecognizeResultTextBox.AppendText(RecognizeResultTextBox.Text + strRecgnResult + " \r\n"); });
+
+        }
+    }
+}
+
